0e3f3564cf1be9ebedb0f1d7a9152eda2ac30764
marcobarrera96 <marcobarrera749@gmail.com>
2020-05-11 17:24:50 -0700

    Add stop times

--- src/components/Po.js
+++ src/components/Po.js
@@ -5,36 +5,91 @@ class Me extends React.Component {
   state = {
     routes: [],
     stops: [],
+    times: [],
   };
 
   componentDidMount() {
+    var line = document.getElementById("line").value;
+
     axios.get("https://api.metro.net/agencies/lametro/routes/").then((res) => {
       const routes = res.data.items;
       this.setState({ routes });
+      console.log(line);
     });
 
     axios
-      .get("https://api.metro.net/agencies/lametro/routes/" + 51 + "/stops/")
+      .get("https://api.metro.net/agencies/lametro/routes/" + 2 + "/stops/")
+      .then((res) => {
+        const stops = res.data.items;
+        this.setState({ stops });
+      });
+
+    axios
+      .get(
+        "https://api.metro.net/agencies/lametro/routes/" +
+          2 +
+          "/stops/" +
+          16291 +
+          "/predictions/"
+      )
+      .then((res) => {
+        const times = res.data.items[0];
+        this.setState({ times });
+      });
+  }
+
+  myU() {
+    var line = document.getElementById("line").value;
+
+    axios
+      .get("https://api.metro.net/agencies/lametro/routes/" + line + "/stops/")
       .then((res) => {
         const stops = res.data.items;
         this.setState({ stops });
       });
   }
 
+  moU() {
+    var line = document.getElementById("line").value;
+    var stop = document.getElementById("stop").value;
+    axios
+      .get(
+        "https://api.metro.net/agencies/lametro/routes/" +
+          line +
+          "/stops/" +
+          stop +
+          "/predictions/"
+      )
+      .then((res) => {
+        const times = res.data.items[0];
+        this.setState({ times });
+      });
+  }
+
   render() {
     return (
       <div>
-        <select className="ui dropdown">
+        <select
+          id="line"
+          className="ui dropdown"
+          onChange={this.myU.bind(this)}
+        >
           {this.state.routes.map((route) => (
-            <option value="">{route.display_name}</option>
+            <option value={route.id}>{route.display_name}</option>
           ))}
         </select>
 
-        <select className="ui dropdown">
+        <select
+          id="stop"
+          className="ui dropdown"
+          onChange={this.moU.bind(this)}
+        >
           {this.state.stops.map((stop) => (
-            <option value="">{stop.display_name}</option>
+            <option value={stop.id}>{stop.display_name}</option>
           ))}
         </select>
+
+        {this.state.times.minutes}
       </div>
     );
   }
