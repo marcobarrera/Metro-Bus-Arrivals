1501ab08570ac36c26055fbc533013b6e0646ac4
Marco Barrera <marcobarrera749@gmail.com>
2021-04-21 17:19:57 -0700

    Destructured props

--- src/components/Dropdowns.js
+++ src/components/Dropdowns.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-export const Dropdowns = (props) => {
+export const Dropdowns = ({subHeader}) => {
 	return (
 		<div className="item">
 			<h2 className="ui header">
-				<div className="sub header">{props.subHeader}</div>
+				<div className="sub header">{subHeader}</div>
 			</h2>
 		</div>
 	);
 };
 
-export const ArrivalTime = (props) => {
+export const ArrivalTime = ({subHeader, description, minutes}) => {
 	return (
 		<div className="item">
 			<div className="ui label large">
 				<h2 className="ui header">
-					<div className="sub header">{props.subHeader}</div>
+					<div className="sub header">{subHeader}</div>
 				</h2>
 				<i className="clock icon large" />
-				{props.description} {props.minutes}
+				{description} {minutes}
 			</div>
 		</div>
 	);
